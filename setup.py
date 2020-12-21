4a65165ff2dc86df9989d99cc8a25695eb35d2d2
Félix An <cutekids100@gmail.com>
2020-12-21 16:06:27 -0500

    fix up setup

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
+import pathlib
 
 setup(
     name="emailsanta",
     version="1.0.0",
     packages=find_packages(exclude=["tests*"]),
-    license="AGPL",
+    license="AGPLv3",
     description="Python module to simulate emailing the legendary Christmas character Santa, powered by Alan Kerr's emailSanta.com.",
-    long_description=open("README.md").read(),
+    long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
     install_requires=["bs4", "html2text", "requests"],
     url="https://github.com/fffelix-jan",
     author="Félix An",
