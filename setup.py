c814620da636ccd3061e65d157fae5357343177b
Félix An <cutekids100@gmail.com>
2020-12-22 15:00:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ GITHUB_URL = "https://github.com/fffelix-jan/emailsanta-py"
 
 setup(
     name="emailsanta",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(exclude=["tests*"]),
     license="AGPLv3",
     description="Python module to simulate emailing the legendary Christmas character Santa, powered by Alan Kerr's emailSanta.com.",
     long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown",
-    install_requires=["bs4", "html2text", "requests"],
+    install_requires=["bs4", "html2text", "lxml", "requests"],
     url=GITHUB_URL,
     author="Félix An",
     author_email="fffelix.jan.yt@gmail.com",
     python_requires=">= 3.5",
         project_urls={
         "Documentation": GITHUB_URL,
-        "Funding": "https://www.felixan.tk",
+        "Funding": "https://paypal.me/fffelixjan",
         "Source": GITHUB_URL,
         "Tracker": "{}/issues".format(GITHUB_URL),
     },
