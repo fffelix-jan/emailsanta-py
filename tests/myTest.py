26e7168d298a2b5c10f75c337983eaca77b319e6
Félix An <cutekids100@gmail.com>
2021-01-04 13:35:19 -0500

    add email and consent to test

--- tests/myTest.py
+++ tests/myTest.py
@@ -1,6 +1,6 @@
 from emailsanta import *
 
 
-myLetter = SantaEmail("Rudolph", 1, 4, "New York", "United States", 3, "carrots", "food", "phone", comment="Hi Santa, this is a test.")
+myLetter = SantaEmail("Rudolph", 1, 4, "New York", "United States", 3, "carrots", "food", "phone", comment="Hi Santa, this is a test.", consent=False, email="test@example.com")
 theReply = SantaReply(myLetter)
 print(theReply.replyText)
